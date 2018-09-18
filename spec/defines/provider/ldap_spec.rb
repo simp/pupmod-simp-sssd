51f2c886ab18b92b5d31d3bf2eb9fbc248a18e10
Adam Yohrling <ayohrling@users.noreply.github.com>
2018-09-18 11:58:32 -0400

    (SIMP-4009) OEL Support for LDAP Provider (#65)
    * (SIMP-4009) OEL Support for LDAP Provider
    
    Changes for the LDAP provider were missed during the last set of
    changes for OEL support.
    
    SIMP-4009 #comment OEL support fixed

--- spec/defines/provider/ldap_spec.rb
+++ spec/defines/provider/ldap_spec.rb
@@ -18,7 +18,7 @@ describe 'sssd::provider::ldap' do
         it { is_expected.to compile.with_all_deps }
 
         it do
-          if ['RedHat','CentOS'].include?(facts[:os][:name]) and facts[:os][:release][:major] < '7'
+          if ['RedHat','CentOS','OracleLinux'].include?(facts[:os][:name]) and facts[:os][:release][:major] < '7'
             ldap_tls_cipher_suite = 'ldap_tls_cipher_suite = HIGH:-SSLv2:-AES128'
           else
             ldap_tls_cipher_suite = 'ldap_tls_cipher_suite = HIGH:-SSLv2'
