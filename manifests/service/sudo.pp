b1b22240e10afe0d9788ce0d878b393a7996699d
Steven Pritchard <steve@sicura.us>
2025-09-24 15:22:50 -0500

    Drop redundant content_only template

--- manifests/service/sudo.pp
+++ manifests/service/sudo.pp
@@ -77,14 +77,7 @@ class sssd::service::sudo (
     # Combine all configuration entries in the expected order
     $_all_entries = $_base_content + $_description_entries + $_debug_level_entries + $_debug_timestamps_entries + $_debug_microseconds_entries + $_sudo_timed_entries
 
-    $_final_content = "${_all_entries.join("\n")}"
-
-    $_content = epp(
-      "${module_name}/content_only.epp",
-      {
-        'content' => $_final_content,
-      },
-    )
+    $_content = "${_all_entries.join("\n")}\n"
   }
 
   sssd::config::entry { 'puppet_service_sudo':
